02d169db3f651f453f5ee4d0f31ffc5f374e1c6b
Pabarope <lhpascoal2@gmail.com>
2018-11-07 00:03:47 -0200

    2push

--- scripts/init.sql
+++ scripts/init.sql
@@ -1,10 +1,8 @@
-create database email_sender;
-
+CREATE DATABASE email_sender;
 \c email_sender
-
-create table emails (
-    id serial not null,
-    data timestamp not null default current_timestamp,
-    assunto varchar(100) not null,
-    mensagem varchar(250) not null
+CREATE TABLE emails (
+ id serial not null,
+ data timestamp not null default current_timestamp,
+ assunto varchar(100) not null,
+ mensagem varchar(250) not null
 );
